cdc5eb0c072179b9d3211e950157fcd303ad417e
line2222 <18103796038@163.com>
2016-05-14 15:37:56 +0800

    commit

---
+++ chk_co.php
@@ -0,0 +1,21 @@
+﻿<?php
+//echo $_POST['user'].'<br>';
+//echo $_POST['pass'].'<br>';
+//echo $_POST['code_num'].'<br>';
+session_start();
+if(isset($_POST['code_num'])&&strlen($_POST['code_num'])>0 &&isset($_SESSION["helloweba_num"]))
+{
+    if($_POST['code_num']==$_SESSION["helloweba_num"])
+    {
+         if($_POST['user']=="admin"&&$_POST['pass']=="123456")
+         {
+              unset($_SESSION["helloweba_num"]);
+              header("location:main.php");
+              exit();
+         }
+    }
+}
+unset($_SESSION["helloweba_num"]);
+header("location:1.html");
+exit();
+?>
